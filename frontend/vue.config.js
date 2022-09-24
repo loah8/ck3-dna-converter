81881071eced19dc2cfbe84107e3959b0b488720
maxby82 <42361686+loah8@users.noreply.github.com>
2022-09-24 23:14:22 +0900

    build option changes

---
+++ frontend/vue.config.js
@@ -0,0 +1,7 @@
+const path = require("path")
+
+module.exports = {
+    outputDir: path.resolve(__dirname, "E:\\WebProjects\\git-repositories\\loah8\\ck3-converter\\"),
+    publicPath: '/ck3-converter/',
+    filenameHashing: false
+}
