028974bb8da38f79feb1c5f87ff0dac19e62f043
maxby82 <42361686+loah8@users.noreply.github.com>
2022-10-15 23:03:49 +0900

    vue-cli-plugin-sitemap add

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -3,5 +3,14 @@ const path = require("path")
 module.exports = {
     outputDir: path.resolve(__dirname, "E:\\WebProjects\\git-repositories\\loah8\\ck3-converter\\"),
     publicPath: '/ck3-converter/',
-    filenameHashing: false
+    filenameHashing: false,
+    pluginOptions : {
+        sitemap: {
+            urls : [
+                'https://loah8.github.io/ck3-converter/'
+            ],
+            pretty: true,
+            outputDir: 'public/'
+        }
+    }
 }
